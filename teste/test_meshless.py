1ec8d361290c24c1bb7153f9f0ffe3ca64f29f98
Bruno Vieira Costa <brunovcosta@outlook.com>
2018-12-16 15:52:09 -0200

    shazan carai!

--- teste/test_meshless.py
+++ teste/test_meshless.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.basis import quadratic_2d
+from src.basis import *
 from src.methods.collocation_method import CollocationMethod
 from src.methods.galerkin_method import GalerkinMethod
 from src.methods.petrov_galerkin_method import PetrovGalerkinMethod
@@ -16,31 +16,31 @@ class TestMeshless(unittest.TestCase):
         sizei = 1
 
         x, y = sp.var("x y")
-        analytical = y-2*x
+        analytical = 18*y-8
 
         def boundary_function(point, check=False):
             if check:
                 return point[0]==sizei or point[1] == sizei or point[0] == size or point[1] == size
             else:
-                if point[0] == sizei:
+                if point[1] == sizei:
                     return sp.lambdify((x,y),analytical,"numpy")(*point)
-                elif point[0] == size:
-                    return sp.lambdify((x,y),analytical,"numpy")(*point)
-                elif point[1] == sizei:
-                    return sp.lambdify((x,y),analytical.diff("x"),"numpy")(*point)
                 elif point[1] == size:
+                    return sp.lambdify((x,y),analytical,"numpy")(*point)
+                elif point[0] == sizei:
+                    return sp.lambdify((x,y),analytical.diff("x"),"numpy")(*point)
+                elif point[0] == size:
                     return sp.lambdify((x,y),analytical.diff("x"),"numpy")(*point)
                 else:
                     raise ValueError("point not in boundary")
 
         def boundary_operator(num, point):
-            if point[0] == sizei:
+            if point[1] == sizei:
                 return num
-            elif point[0] == size:
-                return num
-            elif point[1] == sizei:
-                return num.derivate("x")
             elif point[1] == size:
+                return num
+            elif point[0] == sizei:
+                return num.derivate("x")
+            elif point[0] == size:
                 return num.derivate("x")
             else:
                 raise ValueError("point not in boundary")
@@ -49,7 +49,7 @@ class TestMeshless(unittest.TestCase):
             return num.Sum([exp.derivate("x").derivate("x"), exp.derivate("y").derivate("y")])
 
         def domain_function(point):
-            return num.Function(analytical, name="domain").eval(point)
+            return domain_operator(num.Function(analytical, name="domain"), point).eval(point)
 
         data = Rectangle(sizei, sizei, size, size).cartesian
 
@@ -69,7 +69,9 @@ class TestMeshless(unittest.TestCase):
             point = data[i]
             # print(u)
             # print(sp.lambdify((x,y),analytical,"numpy")(*point))
-            self.assertAlmostEqual(u, sp.lambdify((x,y),analytical,"numpy")(*point), 4)
+            correct = sp.lambdify((x,y),analytical,"numpy")(*point)
+            print(u - correct)
+            self.assertAlmostEqual(u, correct, 4)
 
     def test_collocation(self):
         self.template(CollocationMethod)
