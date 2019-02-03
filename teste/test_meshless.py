04df84750fa1cd0fad9e185bc86102ff32f45fb2
Bruno Vieira Costa <brunovcosta@outlook.com>
2019-02-02 22:37:41 -0200

    fast test full test

--- teste/test_meshless.py
+++ teste/test_meshless.py
@@ -21,8 +21,8 @@ elastic_region_example = Rectangle(
     y1=-15,
     x2=60,
     y2=15,
-    dx=10,
-    dy=7.5,
+    dx=30,
+    dy=15,
     parametric_partition={
         1:    ["NEUMANN",   "NEUMANN"],
         2:    ["NEUMANN",   "NEUMANN"],
@@ -35,8 +35,8 @@ elastic_region_example = Rectangle(
 potential_region_example = Rectangle(
     x1=1,
     y1=1,
-    x2=4,
-    y2=4,
+    x2=3,
+    y2=3,
     parametric_partition={
         1: ["DIRICHLET"],
         2: ["NEUMANN"],
@@ -93,74 +93,6 @@ class TestMeshless(unittest.TestCase):
     def test_petrov_galerkin_elasticity(self):
         self.rectangle_template(PetrovGalerkinMethod, ElasticModel, elastic_region_example)
 
-
-
-fast_elastic_region_example = Rectangle(
-    x1=0,
-    y1=0,
-    x2=2,
-    y2=2,
-    parametric_partition={
-        1:    ["NEUMANN",   "NEUMANN"],
-        2:    ["NEUMANN",   "NEUMANN"],
-        3:    ["NEUMANN",   "NEUMANN"],
-        3.49: ["DIRICHLET", "NEUMANN"],
-        3.5:  ["DIRICHLET", "DIRICHLET"],
-        4:    ["DIRICHLET", "NEUMANN"]
-    })
-
-fast_potential_region_example = Rectangle(
-    x1=0,
-    y1=0,
-    x2=2,
-    y2=2,
-    parametric_partition={
-        1: ["DIRICHLET"],
-        2: ["NEUMANN"],
-        3: ["DIRICHLET"],
-        4: ["NEUMANN"]
-    })
-
-class FastTestMeshless(unittest.TestCase):
-    def rectangle_template(self, method_class, model_class, region):
-        data = region.cartesian
-
-        model = model_class(region=region)
-
-        method = method_class(
-            model=model,
-            basis=quadratic_2d)
-
-        result = method.solve()
-
-        corrects = np.reshape([sp.lambdify((x,y),model.analytical,"numpy")(*point) for point in data], (model.num_dimensions*len(data)))
-        result = result.reshape(model.num_dimensions*len(data))
-
-        diff = corrects - result
-
-    def test_collocation_potential(self):
-        self.rectangle_template(CollocationMethod, PotentialModel, fast_potential_region_example)
-
-    def test_collocation_fast_elasticity(self):
-        self.rectangle_template(CollocationMethod, ElasticModel, fast_elastic_region_example)
-
-    def test_subregion_fast_potential(self):
-        self.rectangle_template(SubregionMethod, PotentialModel, fast_potential_region_example)
-
-    def test_subregion_fast_elasticity(self):
-        self.rectangle_template(SubregionMethod, ElasticModel, fast_elastic_region_example)
-
-    def test_galerkin_fast_potential(self):
-        self.rectangle_template(GalerkinMethod, PotentialModel, fast_potential_region_example)
-
-    def test_galerkin_fast_elasticity(self):
-        self.rectangle_template(GalerkinMethod, ElasticModel, fast_elastic_region_example)
-
-    def test_petrov_galerkin_porential(self):
-        self.rectangle_template(PetrovGalerkinMethod, PotentialModel, fast_potential_region_example)
-
-    def test_petrov_galerkin_fast_elasticity(self):
-        self.rectangle_template(PetrovGalerkinMethod, ElasticModel, fast_elastic_region_example)
 
 if __name__ == '__main__':
     unittest.main()
