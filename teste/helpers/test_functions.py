f3e78387949457970adc48951974897ec298077b
Bruno Vieira Costa <brunovcosta@outlook.com>
2019-03-24 01:54:32 -0300

    test mls2d for crimped analytical function

--- teste/helpers/test_functions.py
+++ teste/helpers/test_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sympy as sp
+import src.helpers.numeric as num
 
 
 class ExampleFunction:
@@ -43,3 +45,33 @@ class TrigonometricExample(ExampleFunction):
 
     def derivate_x(self, point):
         return np.cos(point[1]/100)
+
+class ComplexExample(ExampleFunction):
+    def __init__(self, n):
+        ExampleFunction.__init__(self, n)
+        self.num_dimensions = 2
+        self.E = E = 3e7
+        self.ni = ni = 0.3
+        self.G = G = E/(2*(1+ni))
+        self.p = p = 10000
+        self.D = (E/(1-ni**2))*np.array([[1, ni, 0],
+                                         [ni, 1, 0],
+                                         [0, 0, (1-ni)/2]]).reshape((3,3,1))
+
+        self.ni = np.array([ni])
+
+        self.h = h = 0.12
+        self.I = I = h**3/12
+        self.L = L = 0.48
+        x, y = sp.var("x y")
+        ux = (-p/(6*E*I))*( (6*L-3*x)*x + (2+ni)*(y**2-h**2/4))
+        uy = (p/(6*E*I))*(3*ni*y**2*(L-x) + (4+5*ni)*h**2*x/4 + (3*L-x)*x**2)
+
+        self.analytical = [ux, uy]
+
+    def eval(self, point):
+        return num.Function(self.analytical[0], name="complex example u").eval(point)
+
+    def derivate_x(self, point):
+        return num.Function(self.analytical[0], name="complex example u").derivate("x").eval(point)
+
--- teste/test_mls2d.py
+++ teste/test_mls2d.py
@@ -6,6 +6,7 @@ import teste.helpers.test_functions as ef
 from src.helpers.weights import GaussianWithRadius as Weight
 
 
+size = 50
 class TestMovingLeastSquare2D(unittest.TestCase):
     def template(self,example):
         point = np.array([12.5, 12.5])
@@ -22,20 +23,23 @@ class TestMovingLeastSquare2D(unittest.TestCase):
         self.assertAlmostEqual(mls.r_first(1), np.sqrt(2)/2, 3)
 
         # test valuation
-        self.assertAlmostEqual(approx, desired, 4)
+        self.assertAlmostEqual(approx, desired, 6)
 
 
     def test_polynomial_phi(self):
-       self.template(ef.PolynomialExample(20))
+       self.template(ef.PolynomialExample(size))
 
     def test_linear_phi(self):
-       self.template(ef.LinearExample(20))
+       self.template(ef.LinearExample(size))
 
     def test_exponential_phi(self):
-       self.template(ef.ExponentialExample(20))
+       self.template(ef.ExponentialExample(size))
 
     def test_trigonometric_phi(self):
-       self.template(ef.TrigonometricExample(20))
+       self.template(ef.TrigonometricExample(size))
+
+    def test_complex_phi(self):
+        self.template(ef.ComplexExample(size))
 
 
 if __name__ == '__main__':
